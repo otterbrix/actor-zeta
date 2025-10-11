dba874e5b1b6633da266cbeb27ab3593c071ccb7
kotbegemot <k0tb9g9m0t@gmail.com>
2025-10-11 23:03:05 +0300

    wip

--- header/actor-zeta/base/dispatch_traits.hpp
+++ header/actor-zeta/base/dispatch_traits.hpp
@@ -15,26 +15,40 @@ namespace actor_zeta {
     template<auto MethodPtr>
     using method = method_map_entry<MethodPtr>;
 
-    /// @brief Вспомогательный тип для поиска action_id по методу (с учетом индекса)
-    template<auto MethodPtr, typename MethodList, uint64_t Index = 0>
+    /// @brief Нерекурсивный поиск action_id по методу через variadic expansion
+    namespace detail {
+        // Хелпер для сравнения одного метода (работает только если типы совпадают)
+        template<auto SearchPtr, auto CurrentPtr>
+        struct is_same_method_ptr : std::false_type {};
+
+        // Специализация для одинаковых типов - можем сравнивать значения
+        template<auto Ptr>
+        struct is_same_method_ptr<Ptr, Ptr> : std::true_type {};
+
+        // Нерекурсивный поиск через constexpr цикл
+        template<auto SearchPtr, auto... MethodPtrs>
+        constexpr uint64_t find_method_index_impl() {
+            // Создаем массив совпадений через pack expansion
+            constexpr bool matches[] = {is_same_method_ptr<SearchPtr, MethodPtrs>::value...};
+
+            // Линейный поиск первого совпадения
+            for (std::size_t i = 0; i < sizeof...(MethodPtrs); ++i) {
+                if (matches[i]) {
+                    return static_cast<uint64_t>(i);
+                }
+            }
+            return 0; // Не найден
+        }
+    }
+
+    /// @brief Вспомогательный тип для поиска action_id по методу (нерекурсивная версия)
+    template<auto MethodPtr, typename MethodList>
     struct find_action_id_with_index;
 
-    // Базовый случай - метод не найден
-    template<auto MethodPtr, uint64_t Index>
-    struct find_action_id_with_index<MethodPtr, type_traits::type_list<>, Index> {
-        static constexpr uint64_t value = 0;
-    };
-
-    // Рекурсивный случай - нашли метод
-    template<auto MethodPtr, typename... Rest, uint64_t Index>
-    struct find_action_id_with_index<MethodPtr, type_traits::type_list<method_map_entry<MethodPtr>, Rest...>, Index> {
-        static constexpr uint64_t value = Index;
-    };
-
-    // Рекурсивный случай - метод не совпадает, ищем дальше
-    template<auto MethodPtr, auto OtherMethodPtr, typename... Rest, uint64_t Index>
-    struct find_action_id_with_index<MethodPtr, type_traits::type_list<method_map_entry<OtherMethodPtr>, Rest...>, Index> {
-        static constexpr uint64_t value = find_action_id_with_index<MethodPtr, type_traits::type_list<Rest...>, Index + 1>::value;
+    // Специализация для type_list с извлечением всех MethodPtrs
+    template<auto SearchPtr, auto... MethodPtrs>
+    struct find_action_id_with_index<SearchPtr, type_traits::type_list<method_map_entry<MethodPtrs>...>> {
+        static constexpr uint64_t value = detail::find_method_index_impl<SearchPtr, MethodPtrs...>();
     };
 
     /// @brief Получение action_id для метода из Actor::dispatch_traits
@@ -54,7 +68,7 @@ namespace actor_zeta {
     /// @endcode
     template<typename Actor, auto MethodPtr>
     struct action_id {
-        static constexpr uint64_t value = find_action_id_with_index<MethodPtr, typename Actor::dispatch_traits::methods, 0>::value;
+        static constexpr uint64_t value = find_action_id_with_index<MethodPtr, typename Actor::dispatch_traits::methods>::value;
     };
 
     /// @brief Короткий хелпер для создания message_id из action
@@ -84,47 +98,37 @@ namespace actor_zeta {
         void dispatch_method_impl(ActorPtr* actor, Sender sender, Args&&... args);
     }
 
-    // Рекурсивный случай - проверяем методы по очереди (с индексом для ActionId)
-    template<typename Actor, typename Method, typename MethodList, uint64_t Index = 0>
-    struct runtime_dispatch_with_index;
-
-    // Базовый случай - метод не найден
-    template<typename Actor, typename Method, uint64_t Index>
-    struct runtime_dispatch_with_index<Actor, Method, type_traits::type_list<>, Index> {
-        template<typename ActorPtr, typename Sender, typename... Args>
-        static bool dispatch(Method method, ActorPtr* actor, Sender sender, Args&&... args) {
-            (void)method; (void)actor; (void)sender; (void)sizeof...(args);
-            return false;
-        }
-    };
-
-    // Рекурсивный случай - проверяем первый метод
-    template<typename Actor, typename Method, auto FirstMethodPtr, typename... Rest, uint64_t Index>
-    struct runtime_dispatch_with_index<Actor, Method, type_traits::type_list<method_map_entry<FirstMethodPtr>, Rest...>, Index> {
-        template<typename ActorPtr, typename Sender, typename... Args>
-        static bool dispatch(Method method, ActorPtr* actor, Sender sender, Args&&... args) {
-            // Сравниваем только если сигнатуры совпадают
-            if constexpr (std::is_same<Method, decltype(FirstMethodPtr)>::value) {
-                if (method == FirstMethodPtr) {
-                    // Нашли! Отправляем сообщение с ActionId = Index
-                    detail::dispatch_method_impl<Actor, FirstMethodPtr, Index>(
-                        actor, sender, std::forward<Args>(args)...);
-                    return true;
-                }
+    // Нерекурсивный хелпер для dispatch одного метода
+    template<typename Actor, typename Method, auto MethodPtr, uint64_t Index, typename ActorPtr, typename Sender, typename... Args>
+    static bool try_dispatch_one(Method method, ActorPtr* actor, Sender sender, Args&&... args) {
+        // Сравниваем только если сигнатуры совпадают
+        if constexpr (std::is_same<Method, decltype(MethodPtr)>::value) {
+            if (method == MethodPtr) {
+                // Нашли! Отправляем сообщение с ActionId = Index
+                detail::dispatch_method_impl<Actor, MethodPtr, Index>(
+                    actor, sender, std::forward<Args>(args)...);
+                return true;
             }
-            // Ищем дальше с Index + 1
-            return runtime_dispatch_with_index<Actor, Method, type_traits::type_list<Rest...>, Index + 1>::dispatch(
-                method, actor, sender, std::forward<Args>(args)...);
         }
-    };
+        return false;
+    }
 
-    // Старый runtime_dispatch_helper теперь использует runtime_dispatch_with_index
-    template<typename Actor, typename Method, auto FirstMethodPtr, typename... Rest>
-    struct runtime_dispatch_helper<Actor, Method, type_traits::type_list<method_map_entry<FirstMethodPtr>, Rest...>> {
+    // Нерекурсивная реализация через fold expression
+    template<typename Actor, typename Method, auto... MethodPtrs, typename ActorPtr, typename Sender, typename... Args, std::size_t... Is>
+    static bool dispatch_impl(Method method, ActorPtr* actor, Sender sender, std::index_sequence<Is...>, Args&&... args) {
+        // Используем fold expression с оператором || для short-circuit evaluation
+        return (try_dispatch_one<Actor, Method, MethodPtrs, Is>(method, actor, sender, std::forward<Args>(args)...) || ...);
+    }
+
+    // Специализация runtime_dispatch_helper с извлечением MethodPtrs из type_list
+    template<typename Actor, typename Method, auto... MethodPtrs>
+    struct runtime_dispatch_helper<Actor, Method, type_traits::type_list<method_map_entry<MethodPtrs>...>> {
         template<typename ActorPtr, typename Sender, typename... Args>
         static bool dispatch(Method method, ActorPtr* actor, Sender sender, Args&&... args) {
-            return runtime_dispatch_with_index<Actor, Method, type_traits::type_list<method_map_entry<FirstMethodPtr>, Rest...>, 0>::dispatch(
-                method, actor, sender, std::forward<Args>(args)...);
+            return dispatch_impl<Actor, Method, MethodPtrs...>(
+                method, actor, sender,
+                std::index_sequence_for<method_map_entry<MethodPtrs>...>{},
+                std::forward<Args>(args)...);
         }
     };
 
